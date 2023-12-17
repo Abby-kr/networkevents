f13378e22f828a9fd3da32d14cd889b229d9681a
Abigail <abby.a.otieno@gmail.com>
2023-12-17 16:49:31 +0300

    Render about and home

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
+import Home from "./home"
+import About from "./about";
+import NavBar from './navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+      <NavBar/>
+      <Router>
+        <Routes>
+          <Route path={"/"} element={<Home/>} />
+          <Route path={"/about"} element={<About/>} />
+          {/* <Route path={"/counsellors"} element={<Counsellors/>} />
+          <Route path={"/signup"} element={<SignupForm />} />
+           <Route path={"/events"} element={<EventsList />} />
+             <Route path={"/contact"} element={<Contact />} /> */}
+        </Routes>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
