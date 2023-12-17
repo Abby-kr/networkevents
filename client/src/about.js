4f8fa25f1d57a4e03ae0a71930c35e4b39020526
Abigail <abby.a.otieno@gmail.com>
2023-12-17 16:48:42 +0300

    First draft for home and about

---
+++ client/src/about.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function About(){
+    return (
+    <div>
+        <h1>Get to know us!!</h1>
+        <h2>Our Mission</h2>
+        <p>Our mission is to connect individuals with meaningful events and volunteer opportunities, while providing organizations with a platform to showcase their initiatives. <br></br>
+        We believe in the transformative power of community engagement and the profound impact that can be achieved when passionate individuals and purpose-driven organizations come together.</p>
+        <br></br>
+        <h2>Our Vision</h2>
+        <p>We hope to bring to life a world where everyone has the opportunity to make a positive change. <br></br>
+        Through facilitating connections between organizations and individuals, we can collectively create a more compassionate, vibrant, and harmonious global community.</p>
+        <h2>Join us today!</h2>
+        <p>Whether you're here to find exciting opportunities or promote your organization's initiatives, we welcome you to begin this journey of making a positive impact. <br>
+        </br>Together, we can build a community where every action, big or small, contributes to a brighter future.</p>
+    </div>
+    )
+}
+
+export default About;
---
+++ client/src/home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Home(){
+    return (
+    <div>
+        <h1>Welcome to Big Events!</h1>
+        <p>At Big Events, we believe in the power of community, collaboration, and making a positive impact. <br>
+        </br>Whether you're an organization looking to promote your events or volunteer opportunities <br></br>
+        or an individual eager to find meaningful ways to get involved, you're in the right place.</p>
+        <h3>Are you an individual on the hunt for a project to devote your time to?</h3>
+        <p>Discover volunteer opportunities that align with your skills and passions,<br></br>
+        as well as, learn more about the organizations behind the events and opportunities you're interested in.</p>
+        <h3>Or are you an organization looking for people eager to make the world a better place?</h3>
+        <p>Showcase your events and volunteer opportunities to a diverse community of passionate individuals,and,<br></br>
+        Engage with a community that shares your organization's values and vision.</p>
+    </div>
+    )
+}
+
+export default Home;
